a5502e46ce7f2823aa453278bee137d592a875a9
tlugat <teo.lugat@hetic.net>
2022-10-19 17:53:04 +0200

    feat(window): add window component

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,8 @@
-import { App } from "./components";
+import { App, Window } from "./components";
 
 window.addEventListener("load", App.init);
+const $testContent = document.createElement("h2");
+$testContent.textContent = "test window";
+document
+	.getElementById("test_window")
+	?.addEventListener("click", () => Window.create($testContent));
