7297b50bbf0b1cd77af3565f23d80b3e8dfe5c08
tlugat <teo.lugat@hetic.net>
2023-02-26 13:13:27 +0100

    ✨ Add date and status items to toolbar

--- src/components/Toolbar.ts
+++ src/components/Toolbar.ts
@@ -1,13 +1,24 @@
 export default class Toolbar {
 	private toolbarElement: HTMLElement;
+	private toolbarStateElement: HTMLElement;
+	private toolbarDateElement: HTMLElement;
 
 	constructor() {
 		this.toolbarElement = document.createElement("div");
 		this.toolbarElement.classList.add("toolbar");
+		this.toolbarStateElement = document.createElement("div");
+		this.toolbarDateElement = document.createElement("div");
 
-		this.updateTime();
+		this.toolbarDateElement.classList.add("date");
+		this.toolbarStateElement.classList.add("state");
+
+		this.toolbarElement.append(
+			this.toolbarDateElement,
+			this.toolbarStateElement
+		);
 		setInterval(() => {
 			this.updateTime();
+			this.updateState();
 		}, 1000);
 
 		document.body.appendChild(this.toolbarElement);
@@ -19,11 +30,60 @@ export default class Toolbar {
 
 	private updateTime() {
 		const date = new Date();
-		const time = date.toLocaleTimeString();
-		const dateStr = date.toLocaleDateString();
-		this.toolbarElement.innerHTML = `
-        <span class="toolbar-time">${time}</span>
-        <span class="toolbar-date">${dateStr}</span>
-      `;
+		const options = {
+			day: "2-digit",
+			month: "2-digit",
+			year: "2-digit",
+		};
+		const dateStr = date.toLocaleDateString("fr-FR", options);
+		const time = date.toLocaleTimeString("fr-FR", {
+			hour12: false,
+			hour: "2-digit",
+			minute: "2-digit",
+		});
+
+		this.toolbarDateElement.innerHTML = `
+            <span class="toolbar-date">${dateStr}</span>
+            <span class="toolbar-time">${time}</span>
+        `;
+	}
+
+	private async updateState() {
+		const isVibrating = navigator.vibrate ? navigator.vibrate(0) : false;
+		const latency = this.getLatency();
+		const battery = await this.getBattery();
+		this.toolbarStateElement.innerHTML = `
+        <span class="toolbar-vibration">${
+					isVibrating ? "📳 Actif" : "📳 Inactif"
+				}</span>
+        <span class="toolbar-battery">🔋 ${
+					battery.level * 100 ?? "unknown"
+				}%</span>
+        <span class="toolbar-latency">📶 ${latency}ms</span>`;
+	}
+
+	private getLatency() {
+		let latency = 0;
+
+		const observer = new PerformanceObserver((list) => {
+			const entries = list.getEntries() as PerformanceNavigationTiming[];
+			for (const entry of entries) {
+				if (entry.name === "navigation") {
+					latency = entry.responseEnd - entry.requestStart;
+				}
+			}
+		});
+
+		observer.observe({ type: "navigation", buffered: true });
+
+		return latency;
+	}
+
+	async getBattery() {
+		try {
+			return await navigator.getBattery();
+		} catch (error) {
+			console.log(error);
+		}
 	}
 }
