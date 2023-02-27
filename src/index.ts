0ac1eed22ca4a00a9824d01f34ee138a080d72ad
Ali Fatoorifar <ali.fatoori@gmail.com>
2023-02-27 19:56:32 +0100

    Add calculator

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Desktop from "./components/Desktop";
 import SettingsManager from "./components/SettingsManager";
 import "./styles/main.scss";
+import Calculator from "./components/Calculator";
 
 const desktop = new Desktop();
 
@@ -9,8 +10,15 @@ windowContent1.textContent = "It works 1";
 
 const settingsManager = new SettingsManager();
 
+const calculator = new Calculator();
+
 const windowContent2 = document.createElement("div");
 windowContent2.textContent = "It works 2";
+
+const windowContent3 = document.createElement("div");
+windowContent3.textContent = "Calculatrice";
+
+
 
 desktop.createWindow({
 	title: "Paramètres",
@@ -28,3 +36,15 @@ desktop.createWindow({
 	height: 250,
 	content: windowContent2,
 });
+
+desktop.createWindow({
+	title: "Calulator",
+	x: 100,
+	y: 100,
+	width: 550,
+	height: 620,
+	content: calculator.createCalculator(),
+});
+
+
+
