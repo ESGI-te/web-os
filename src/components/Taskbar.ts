9beab22aff6878c5b767dcdb35c34ae434220b17
tlugat <teo.lugat@hetic.net>
2023-02-27 23:13:04 +0100

    ✨ Add window focus custom event

--- src/components/Taskbar.ts
+++ src/components/Taskbar.ts
@@ -11,11 +11,16 @@ export default class Taskbar {
 		this.windows = [];
 
 		document.body.appendChild(this.taskbarElement);
+
+		window.addEventListener<any>("windowFocus", (event: CustomEvent) =>
+			this.setWindowActive(event.detail.id)
+		);
 	}
 
 	public addWindow(window: Window) {
 		const button = document.createElement("button");
 		button.classList.add("taskbar-item");
+		button.id = window.getId();
 		button.innerText = window.getTitle();
 
 		button.addEventListener("click", () => {
@@ -29,4 +34,15 @@ export default class Taskbar {
 		this.taskbarElement.appendChild(button);
 		this.windows.push(window);
 	}
+
+	private setWindowActive = (id: string) => {
+		this.windows.forEach((window) => {
+			if (window.getId() === id) {
+				document
+					.querySelectorAll(".taskbar-item")
+					.forEach((w) => w.classList.remove("active"));
+				document.querySelector(`.taskbar-item#${id}`)?.classList.add("active");
+			}
+		});
+	};
 }
